43cace97e713fa5f49ff8f1333c94156ca860b1c
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-05-06 14:42:12 +0300

    Add "Mark and Toys" solution
    This commit includes the addition of a new F# project "Mark and Toys". This project contains a solution to the problem of how many toys Mark can buy with the available funds. The implementation takes into account the constraint that each toy can only be purchased once.

---
+++ Mark and Toys/Program.fs
@@ -0,0 +1,37 @@
+﻿(*
+    Mark and Jane are very happy after having their first child.
+    Their son loves toys, so Mark wants to buy some.
+    There are a number of different toys lying in front of him,
+    tagged with their prices. Mark has only a certain amount to spend,
+    and he wants to maximize the number of toys he buys with this money.
+    Given a list of toy prices and an amount to spend,
+    determine the maximum number of gifts he can buy.
+
+    Note Each toy can be purchased only once. 
+*)
+
+let toysCount, availableFunds =
+    System.Console.ReadLine().Split(' ')
+    |> Seq.map int
+    |> Seq.toList
+    |> fun x -> x[0], x[1]
+
+let canBuy price = price <= availableFunds
+
+let toyPrices =
+    System.Console.ReadLine().Split(' ')
+    |> Seq.truncate toysCount
+    |> Seq.map int
+    |> Seq.filter canBuy
+    |> Seq.sort
+    |> Seq.toList
+
+let countToys prices funds =
+    let rec loop prices total count =
+        match prices with
+        | price :: rest when total + price <= funds -> loop rest (total + price) (count + 1)
+        | _ -> count
+
+    loop prices 0 0
+
+printfn $"%d{countToys toyPrices availableFunds}"
