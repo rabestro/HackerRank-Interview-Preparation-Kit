162131e88adb92c5fa660e5baae2d3a7def9ea75
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-05-07 18:05:58 +0300

    Added "Two Strings" project to the solution
    A new project named "Two Strings" was added to the solution. It includes an implementation for a function that checks if two strings share a common substring. The project configuration and the program script are included in the new project.

---
+++ Two Strings/Program.fs
@@ -0,0 +1,16 @@
+﻿(*
+    Given two strings, determine if they share a common substring.
+    A substring may be as small as one character. 
+*)
+
+let testCases = System.Console.ReadLine() |> int
+
+let hasCommonSubstring (s1: string) (s2: string) =
+    let firstSet = s1 |> Set.ofSeq
+    if Seq.exists firstSet.Contains s2 then "YES" else "NO"
+
+Seq.initInfinite (fun _ -> System.Console.ReadLine())
+|> Seq.chunkBySize 2
+|> Seq.take testCases
+|> Seq.map (fun chunk -> hasCommonSubstring chunk.[0] chunk.[1])
+|> Seq.iter (printfn "%s")
